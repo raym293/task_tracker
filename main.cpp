10900a311cac15e6215c0ac7d111552e90125c05
raym293 <f20230905@goa.bits-pilani.ac.in>
2025-06-23 17:42:08 +0530

    implemented view as well as it made it default

--- main.cpp
+++ main.cpp
@@ -14,20 +14,34 @@ using json = nlohmann::json;
 // createdAt: The date and time when the task was created
 // updatedAt: The date and time when the task was last updated
 
-typedef struct {
+struct Task {
     std::string description;
     std::string status;
-
-
-}Task;
+    time_t timeCreated;
+    time_t timeUpdated;
+    Task(json obj) : description(obj["description"]), status(obj["status"]) {
+        time_t temp;
+        time(&temp);
+        timeCreated = timeUpdated = temp;
+    } 
+    void Print() {
+        std::cout << description << '\n';
+        std::cout << status << '\n';
+        std::cout << ctime(&timeCreated);
+        std::cout << ctime(&timeUpdated) << '\n';
+    }
+};
 
 int main(int argc, char *argv[]) {
-    // if(argc == 1) {
-    //     std::cout << "Please add an argument\n";
-    //     return 0;
-    // }
-    time_t t;
-    time(&t);
-    std::cout << ctime(t) << '\n';
+    std::ifstream f("data.json");
+    json data = json::parse(f);
+    int numberOfTasks = data.size();
+    std::vector<Task> tasks;
+    for(int i = 1; i <= numberOfTasks; i++) {
+        tasks.push_back(Task(data[std::to_string(i)]));
+    }
+    if(argc == 1 || (std::string)argv[1] == "view") {
+        for(auto i:tasks) i.Print();
+    }
     return 0;
 }
